5a7f2a2157ba17af6742e0e794c9278a8d2db7e6
Rana Khawar Ali <138607670+khawarrustam@users.noreply.github.com>
2026-09-16 03:11:53 -0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int , int>mp;
+
+        for(int i = 0 ; i<nums.size() ; i++){
+            mp[nums[i]] = i ;
+        }
+
+        for(int i = 0 ; i<nums.size() ;i++){
+            int temp = target - nums[i];
+            if(mp.count(temp) && mp[temp] != i){
+                return {i,mp[temp]};
+            }
+        }
+
+        return {};
+    }
+};
